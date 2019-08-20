57ca7a73fb3a7368e08c51c6c0de316763cec049
Pat Palmerston <patpalmerston@gmail.com>
2019-08-20 14:32:55 -0700

    react router dom added with private route and utils

--- Auth-Friends-master/auth-friends/src/App.js
+++ Auth-Friends-master/auth-friends/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './components/auth/Login'
+import Home from './components/pages/Home';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import './App.css';
 
 function App() {
-	return <div className='App'>APP</div>;
+	return(
+
+	<Router>
+		<Fragment>
+			<div className='App'>
+        <Switch>
+        <PrivateRoute exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+
+        </Switch>
+      </div>;
+		</Fragment>
+	</Router>
+  )
 }
 
 export default App;
